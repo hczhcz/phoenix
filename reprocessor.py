543727ed81d213610b644a9d363c3b51624584ee
hcz <h.cz@qq.com>
2013-07-10 00:01:21 +0800

    添加正则处理单元

---
+++ reprocessor.py
@@ -0,0 +1,13 @@
+import re
+
+def MakeRe(rule):
+    pass
+
+def Process(reobj, data):
+    pass
+
+def ProcessFile(reobj, infile, outfile):
+    pass
+
+def ProcessDir(reobj, indir, outdir):
+    pass
