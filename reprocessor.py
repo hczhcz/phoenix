319f44f422d5d46a90238679c3abdc8dca4496d1
hcz <h.cz@qq.com>
2013-07-10 21:55:33 +0800

    正则单元推进

--- reprocessor.py
+++ reprocessor.py
@@ -1,10 +1,22 @@
 import re
 
-def MakeRe(rule):
-    pass
+# reobj = re.compile(regex)
+# result = reobj.sub(newstring, subject)
+# re.sub(regex, newstring, subject)
+
+_ReContainer = []
+
+def AddRe(rulefrom, ruleto):
+    _ReContainer.append((re.compile(rulefrom), ruleto))
+
+def ClearRe:
+    _ReContainer = []
 
 def Process(reobj, data):
-    pass
+    result = data
+    for reobj, tostr in _ReContainer:
+        result = reobj.sub(tostr, result)
+    return result
 
 def ProcessFile(reobj, infile, outfile):
     pass
